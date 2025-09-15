d51407f787e27e4d00c8b0bb8f6f9c0ebf67ee74
Raz Ahamed <raz.abcoder@gmail.com>
2025-09-15 11:03:27 +0600

    Login API

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -25,10 +25,24 @@ const registerUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
+    if (!req.body) {
+        return res.status(400).json({
+            message: 'Invalid Data',
+        });
+    }
     const client = await connectDB();
     const collection = client.db('api_project').collection('users');
-    const users = await collection.find({ 'email': email, 'password': password }).toArray();
-    console.log(users);
+    const users = await collection.findOne({ 'email': email, 'password': password });    
+    if(users) {
+        res.status(200).json({                  
+            message: 'Login Successful',
+            user: users
+        });
+    } else {
+        res.status(401).json({
+            message: 'Invalid Credentials'
+        });                     
+    }
 };
 
 module.exports = { registerUser, loginUser };
