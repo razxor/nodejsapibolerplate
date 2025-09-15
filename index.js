ec146bab45f44b3521a39a0b04465009bacd4db7
Raz Ahamed <raz.abcoder@gmail.com>
2025-09-15 08:49:51 +0600

    .env added and PORT

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const exprees = require('express');
 const app = exprees();
+const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });     
-app.listen(4000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 4000');
 })
